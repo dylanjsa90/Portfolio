c51ece6e3bd5f03d7b55ed5f42e60c1bad663d98
dylanjsa90 <dylanjsanders1@gmail.com>
2016-11-03 01:47:17 -0700

    overhauled the styling, fixed the nav bar for better responsive UI, did a general overhaul of the layout classes and changed webpack to compile into docs

--- app/components/nav_bar/index.js
+++ app/components/nav_bar/index.js
@@ -6,12 +6,32 @@ module.exports = function(app) {
     template: require('./nav_bar_template.html'),
   });
 
-  app.controller('NavController', function() {
+  app.controller('NavController', ['$location', function($location) {
     this.isCollapsed = true;
     this.collapsedClass = this.isCollapsed ? 'collapse': '';
     this.toggle = function() {
       this.isCollapsed = !this.isCollapsed;
       this.collapsedClass = this.isCollapsed ? 'collapse': '';
     };
-  });
+    this.home = true;
+    this.projects = false;
+    this.about = false;
+    this.contact = false; 
+
+    this.currentTab = function() {
+      this.setToFalse();
+      if ($location.url().includes('home')) this.home = true;
+      if ($location.url().includes('about')) this.about = true;
+      if ($location.url().includes('projects')) this.projects = true;
+      if ($location.url().includes('contact')) this.contact = true; 
+    };
+
+    this.setToFalse = function() {
+      this.home = false;
+      this.projects = false;
+      this.about = false;
+      this.contact = false;
+    };
+
+  }]);
 };
