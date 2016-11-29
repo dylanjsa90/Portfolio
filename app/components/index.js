60fea51a8e6df7eaf816b3ae1db0d362e2945b46
dylanjsa90 <dylanjsanders1@gmail.com>
2016-11-28 17:10:00 -0800

    updated some styling and changed about bio&skills to tabs

--- app/components/index.js
+++ app/components/index.js
@@ -4,5 +4,6 @@ module.exports = function(app) {
   require('./nav_bar')(app);
   require('./projects')(app);
   require('./contact')(app);
+  require('./about')(app);
   require('./footer_component')(app);
 };
